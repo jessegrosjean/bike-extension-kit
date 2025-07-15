1607aa9e0d385b601b72c4f3dcced50e0f649586
Jesse Grosjean <jesse@hogbaysoftware.com>
2025-07-15 11:33:03 -0400

    update to use command context

--- src/calendar.bkext/app/commands.ts
+++ src/calendar.bkext/app/commands.ts
@@ -1,43 +1,33 @@
+import { CommandContext } from 'bike/app'
 import { getDayRow, getMonthRow, getYearRow } from './calendar-rows'
 
-export function yearCommand(): boolean {
-  let editor = bike.frontmostWindow?.outlineEditors[0]
-  if (!editor) {
-    return true
-  }
-
+export function yearCommand(context: CommandContext): boolean {
+  let editor = context.editor
+  if (!editor) return true
   editor.outline.transaction({ animate: 'default' }, () => {
     let outline = editor.outline
     let yearRow = getYearRow(outline, new Date(), true, true)
     editor.focus = yearRow
     editor.selectCaret(yearRow.firstChild!, 0)
   })
-
   return true
 }
 
-export function monthCommand(): boolean {
-  let editor = bike.frontmostWindow?.outlineEditors[0]
-  if (!editor) {
-    return true
-  }
-
+export function monthCommand(context: CommandContext): boolean {
+  let editor = context.editor
+  if (!editor) return true
   editor.outline.transaction({ animate: 'default' }, () => {
     let outline = editor.outline
     let monthRow = getMonthRow(outline, new Date(), true)
     editor.focus = monthRow
     editor.selectCaret(monthRow.firstChild!, 0)
   })
-
   return true
 }
 
-export function todayCommand(): boolean {
-  let editor = bike.frontmostWindow?.currentOutlineEditor
-  if (!editor) {
-    return true
-  }
-
+export function todayCommand(context: CommandContext): boolean {
+  let editor = context.editor
+  if (!editor) return true
   editor.outline.transaction({ animate: 'default' }, () => {
     let outline = editor.outline
     let todayRow = getDayRow(outline, new Date())
@@ -47,6 +37,5 @@ export function todayCommand(): boolean {
     editor.focus = todayRow
     editor.selectCaret(todayRow.firstChild!, 0)
   })
-
   return true
 }
