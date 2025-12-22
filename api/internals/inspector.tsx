298c14a1f1b8af217a668302c10428d34eba8138
Jesse Grosjean <jesse@hogbaysoftware.com>
2025-12-22 07:48:55 -0500

    Add padding above inspector

--- api/internals/inspector.tsx
+++ api/internals/inspector.tsx
@@ -4,6 +4,11 @@ declare global {
   }
 }
 
+// Add padding above the inspector
+const style = document.createElement('style')
+style.textContent = 'body { padding-top: 32px; }'
+document.head.appendChild(style)
+
 window.BikeInspector = {}
 
 export {}
