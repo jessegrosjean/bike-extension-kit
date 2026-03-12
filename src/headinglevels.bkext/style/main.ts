347c080d2f4132a824bcc9b3719de7c300b47824
Jesse Grosjean <jesse@hogbaysoftware.com>
2026-03-12 14:58:37 -0400

    Rules for each heading level

--- src/headinglevels.bkext/style/main.ts
+++ src/headinglevels.bkext/style/main.ts
@@ -4,6 +4,21 @@ let modifier = defineEditorStyleModifier('headinglevels', 'Heading Levels')
 
 modifier.layer('row-formatting', (row) => {
   row(`.heading level() = 1`, (context, row) => {
-    row.text.color = Color.systemRed()
+    row.text.color = context.theme.colors.get('heading1')
+  })
+  row(`.heading level() = 2`, (context, row) => {
+    row.text.color = context.theme.colors.get('heading2')
+  })
+  row(`.heading level() = 3`, (context, row) => {
+    row.text.color = context.theme.colors.get('heading3')
+  })
+  row(`.heading level() = 4`, (context, row) => {
+    row.text.color = context.theme.colors.get('heading4')
+  })
+  row(`.heading level() = 5`, (context, row) => {
+    row.text.color = context.theme.colors.get('heading5')
+  })
+  row(`.heading level() = 6`, (context, row) => {
+    row.text.color = context.theme.colors.get('heading6')
   })
 })
